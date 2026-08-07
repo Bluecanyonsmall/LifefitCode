6edb3d7e62644ed4046f197b29d1543ea58f8d47
unknown <bluecanyonsmall@gmail.com>
2026-08-07 19:56:09 +0900

    Test Playwright

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 from playwright.sync_api import sync_playwright
+import time
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
     page = browser.new_page()
 
-    page.goto(
-        "https://gamerch.com/invitation/940697",
-        wait_until="domcontentloaded",
-        timeout=60000
-    )
+    page.goto("https://gamerch.com/invitation/940697")
+    time(10)
 
-    html = page.content()
-
-    with open("page.html", "w", encoding="utf-8") as f:
-        f.write(html)
-
-    print("HTML保存完了")
+    textarea = page.locator('textarea[name="body"]')
+    print("textareaの数:", textarea.count())
 
     browser.close()
