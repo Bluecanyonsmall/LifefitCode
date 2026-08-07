fa30b07ec4c6ab2f89f04980ef354ca47fbfe9f2
unknown <bluecanyonsmall@gmail.com>
2026-08-07 19:33:20 +0900

    Test Playwright

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello GitHub Actions!")
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+
+    page = browser.new_page()
+
+    page.goto("https://gamerch.com/invitation/940697")
+
+    print(page.title())
+
+    browser.close()
