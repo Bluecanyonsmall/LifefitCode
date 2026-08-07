23cf1ff447236ea9589d477e2a49d72720b242dd
unknown <bluecanyonsmall@gmail.com>
2026-08-07 19:44:05 +0900

    Test Playwright

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 from playwright.sync_api import sync_playwright
 
-MESSAGE = "FR3U94TR9U"
-
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
 
     page = browser.new_page()
 
-    page.goto("https://gamerch.com/invitation/940697")
+    page.goto(
+        "https://gamerch.com/invitation/940697",
+        wait_until="domcontentloaded",
+        timeout=60000
+    )
 
-    page.wait_for_load_state("networkidle")
-
-    page.locator('textarea[name="body"]').fill(MESSAGE)
-
-    page.get_by_role("button", name="投稿する").click()
-
-    page.wait_for_timeout(5000)
+    print(page.title())
 
     browser.close()
